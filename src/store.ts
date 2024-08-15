8fbc9fa89fb063c805fa6f528cffb30baa0b477d
kevinjg16 <desarrollo@solar-center.mx>
2024-08-15 17:28:50 -0600

    Add details order

--- src/store.ts
+++ src/store.ts
@@ -5,21 +5,76 @@ import { Product } from "@prisma/client";
 interface Store {
   order: OrderItem[];
   addToOrder: (product: Product) => void;
+  increaseQuantity: (id: Product["id"]) => void;
+  decrementQuantity: (id: Product["id"]) => void;
+  removeItem: (id: Product["id"]) => void;
+  clearOrder: () => void;
 }
 
-export const useStore = create<Store>((set) => ({
+export const useStore = create<Store>((set, get) => ({
   order: [],
   addToOrder: (product) => {
     const { categoryId, image, ...data } = product;
-    set((state) => ({
-      order: [
-        ...state.order,
+    let order: OrderItem[] = [];
+    if (get().order.find((item) => item.id === product.id)) {
+      order = get().order.map((item) =>
+        item.id === product.id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+              subtotal: item.price * (item.quantity + 1),
+            }
+          : item
+      );
+    } else {
+      order = [
+        ...get().order,
         {
           ...data,
           quantity: 1,
           subtotal: 1 * product.price,
         },
-      ],
+      ];
+    }
+
+    set(() => ({
+      order,
+    }));
+  },
+  increaseQuantity: (id) => {
+    set((state) => ({
+      order: state.order.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+              subtotal: item.price * (item.quantity + 1),
+            }
+          : item
+      ),
+    }));
+  },
+  decrementQuantity: (id) => {
+    set((state) => ({
+      order: state.order.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity: item.quantity - 1,
+              subtotal: item.price * (item.quantity - 1),
+            }
+          : item
+      ),
+    }));
+  },
+  removeItem: (id) => {
+    set((state) => ({
+      order: state.order.filter((item) => item.id !== id),
+    }));
+  },
+  clearOrder: () => {
+    set(() => ({
+      order: [],
     }));
   },
 }));
