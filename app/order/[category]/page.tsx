6186f69ac4fbceaf236414f952870befcf02d3eb
kevinjg16 <desarrollo@solar-center.mx>
2024-08-16 17:21:00 -0600

    Add product category table

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from "@/components/products/ProductCard";
+import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
 async function getProducts(category: string) {
@@ -20,6 +21,7 @@ export default async function OrderPage({
   const products = await getProducts(params.category);
   return (
     <>
+      <Heading>Eligue y personaliza tu pedido</Heading>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
