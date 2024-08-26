ac762f3ea3877f757841b6e1cd1870ff6f5b86b4
kevinjg16 <desarrollo@solar-center.mx>
2024-08-26 13:03:23 -0600

    Add new product

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { formatCurrency } from "@/src/utils";
+import { formatCurrency, getImagePath } from "@/src/utils";
 import { Product } from "@prisma/client";
 import Image from "next/image";
 import AddProductButton from "./AddProductButton";
@@ -8,23 +8,23 @@ type ProductCardProps = {
 };
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const imagePath = getImagePath(product.image);
   return (
     <div className="border bg-white rounded-2xl h-full mt-4">
       <Image
         className="rounded-t-2xl"
         width={400}
         height={500}
-        src={`/products/${product.image}.jpg`}
+        src={imagePath}
         alt={`Imagen platillo ${product.name}`}
         quality={75}
       />
-
       <div className="p-5">
         <h3 className="text-2xl font-bold"> {product.name} </h3>
         <p className=" font-black mt-4 text-4xl text-amber-500">
           {formatCurrency(product.price)}
         </p>
-        <AddProductButton product={product}/>
+        <AddProductButton product={product} />
       </div>
     </div>
   );
